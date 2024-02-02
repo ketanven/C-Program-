c182e72925a20f4598ab4b4d609d8fd5a10a36de
Ketan Venkatesh Pillai <98447291+ketanven@users.noreply.github.com>
2024-02-02 12:12:55 +0530

    Add files via upload

---
+++ areaofcircle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float circle,r;
+	
+	printf("PLEASE ENTER THE VALUE OF REDIUS\n");
+	scanf("%f",&r);
+	
+	circle = 3.14*r*r;
+	
+	printf("AREA OF CIRCLE IS %.2f",circle);
+}
---
+++ helloworld.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main(){
+	
+	printf("HELLO WORLD""\n");
+	int a =50;
+	int b=90;
+	int c=100;
+	int d=a*b/c;
+	printf("%d",d);
+	
+}
+    
